b9a4624a6a6b5a3437d627ea27171ddef874e10f
atlas <a.sproson@outlook.com>
2022-06-25 13:48:45 +0100

    Adds graphql call logic and calls getUser component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
+import GetUsers from './components/GetUsers';
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+const link = from([
+  errorLink,
+  new HttpLink({uri: "http://localhost:8080/graphql"})
+])
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>React GraphQL Apollo</h1>
-    </div>
+    <ApolloProvider client={client}>
+        <GetUsers />
+    </ApolloProvider>
   );
 }
 
